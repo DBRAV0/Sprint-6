6b68801827f1ed0f81a162901f2b4471df535692
DBRAV0 <94563780+DBRAV0@users.noreply.github.com>
2022-02-22 20:28:08 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function aumento1(imagen) {
+	imagen.style="border:2px solid black;width:400px;height:400px;border-radius:60px";
+}
+function aumento2(imagen) {
+	imagen.style="border:2px solid black;width:350px;height:350px;border-radius:220px";
+}
+function normal(imagen) {
+	imagen.style="";
+}
